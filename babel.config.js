f948ae9e2608f4e6b6792775c7e1b2b7b55d8e45
Graham Barber <grahammbarber@gmail.com>
2018-12-08 11:18:35 -0800

    chore(deps): add emotion-theming, babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ['next/babel']
+  env: {
+    production: {
+      plugins: ['next/babel', 'emotion']
+    },
+    development: {
+      plugins: [['next/babel', 'emotion', { sourceMap: true }]]
+    }
+  }
 }
