8a35f5d245b8c830fdea6366a9a5e965793951e0
Graham Barber <grahammbarber@gmail.com>
2018-12-08 12:35:18 -0800

    feat(document): add custom document

---
+++ frontend/pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps (ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render () {
+    return (
+      <html>
+        <Head>
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link
+            href='https://fonts.googleapis.com/css?family=Overpass|Overpass+Mono'
+            rel='stylesheet'
+          />
+          <style>{`body { margin: 0 }`}</style>
+        </Head>
+        <body className='custom_class'>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
