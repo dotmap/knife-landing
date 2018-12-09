ec37d9c00448fde77a1890e5e5eb3877d92c610e
Graham Barber <grahammbarber@gmail.com>
2018-12-08 16:57:08 -0800

    fix(navbar): remove create event button

--- frontend/components/navbar/index.jsx
+++ frontend/components/navbar/index.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { Flex, Heading, Button, Text } from '@rebass/emotion'
 
 export default () => (
-  <Flex as='nav' p={3}>
+  <Flex as='nav' p={[3, 4]}>
     <Flex width={1 / 3} alignItems='center'>
       <Link href='/'>
         <a style={{ textDecoration: 'none', color: 'black' }}>
@@ -11,15 +11,8 @@ export default () => (
       </Link>
     </Flex>
     <Flex width={2 / 3} justifyContent='flex-end'>
-      <Link href='/create'>
-        <a>
-          <Button bg='MediumSpringGreen' mx={3} color='black' border='3px solid black'>
-            <Text fontFamily='inter'>Create Your Event</Text>
-          </Button>
-        </a>
-      </Link>
       <a>
-        <Button bg='magenta' color='black' border='3px solid black'>
+        <Button bg='white' color='black' border='3px solid black'>
           <Text fontFamily='inter'>Sign In</Text>
         </Button>
       </a>
--- frontend/pages/create/create.spec.jsx
+++ frontend/pages/create/create.spec.jsx
@@ -3,11 +3,11 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import AboutPage from '.'
+import CreatePage from '.'
 
 describe('About page', () => {
   it('matches snapshot', () => {
-    const component = renderer.create(<AboutPage />)
+    const component = renderer.create(<CreatePage />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
