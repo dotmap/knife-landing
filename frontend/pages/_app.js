467d92fe0c9d0ab76ac506528ee518fbb2d01ca1
Graham Barber <grahammbarber@gmail.com>
2018-12-16 13:54:03 -0800

    refactor(app): correct colors to match brand

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -10,9 +10,9 @@ const Highlight = styled.div`
   background: rgb(76, 255, 190);
   background: linear-gradient(
     35deg,
-    rgba(76, 255, 190, 1) 5%,
-    rgba(19, 85, 255, 1) 50%,
-    rgba(255, 19, 175, 1) 95%
+    rgba(0, 255, 162, 1) 5%,
+    rgba(0, 71, 255, 1) 50%,
+    rgba(255, 0, 168, 1) 95%
   );
   height: 20px;
   width: 100%;
